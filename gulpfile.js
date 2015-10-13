9794aaef29bde523472fa33d456b27b55c6976ba
Ho-Yang Tsai <hoyang.t@gmail.com>
2015-10-13 10:17:45 +0800

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ var notify = require('gulp-notify');
 var webpackConfig = require('./webpack.config.js');
 
 gulp.task('webpack', function() {
-    return gulp.src('./app/App.js')
-        .pipe(webpack(webpackConfig), null,
-          function(err, stats) {
-            notify.onError("Error: <%= err %>");
-          })
-        .pipe(gulp.dest('.'))
-        .pipe(livereload());;
+  return gulp.src('./app/App.js')
+    .pipe(webpack(webpackConfig), null,
+      function(err, stats) {
+        notify.onError("Error: <%= err %>");
+      })
+    .pipe(gulp.dest('.'))
+    .pipe(livereload());
 });
 
 gulp.task('watchIndex', function() {
@@ -23,9 +23,9 @@ gulp.task('watchIndex', function() {
 });
 
 gulp.task('watch', function() {
-    livereload.listen({ start: true });
-    gulp.watch(['app/*.js', 'app/**/*.js'], ['webpack']);
-    gulp.watch('public/index.html', ['watchIndex']);
+  livereload.listen({ start: true });
+  gulp.watch(['app/*.js', 'app/**/*.js'], ['webpack']);
+  gulp.watch('public/index.html', ['watchIndex']);
 });
 
 gulp.task('default', ['webpack','watch']);
